dc6598681d61b26d4737f73e5e2bb7f2ffa1de33
philles85 <philster04@outlook.com>
2025-01-12 01:38:35 +0100

    Tabell funktionen
    Här börjar jag på funktionen till tabellen, där jag loopar igenom row och column för att sedan skapa en id för varje div för att hålla reda på row och column per div. Här appendar jag div och en skapar en class för att styla med css.

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ let currentPlayer = playerOne;
 
 let nRow = 6;
 let nCol = 7;
+
+
+function loadTable() {
+    for (let r = 0; r < nRow; r++) {
+        for (let c = 0; c < nCol; c++) {
+            let coins = document.createElement("div");
+            coins.id = r.toString() + c.toString();
+            coins.classList.add("coins");
+            document.getElementById("table").appendChild(coins);
+        }
+    }
+}
