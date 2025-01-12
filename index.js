aa0eee900d50a20ee807d8b38531d3915e921721
philles85 <philster04@outlook.com>
2025-01-12 01:47:04 +0100

    Klick funktionen för rutorna
    Här skapar jag en addventlistner till rutorna, när man klickar på en kolumn hamnar poletten längst ner, genom column array för alla columner och dess row nivå, och ändrar currentplayer till rätt färg efter varje klickning. För att flytta upp efter varje klick så subtraherar vi columns med 1 varje gång för att flytta oss upp i raden.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let currentPlayer = playerOne;
 
 let nRow = 6;
 let nCol = 7;
+let columns = [5, 5, 5, 5, 5, 5, 5]
 
 
 function loadTable() {
@@ -14,6 +15,28 @@ function loadTable() {
             coins.id = r.toString() + c.toString();
             coins.classList.add("coins");
             document.getElementById("table").appendChild(coins);
+            coins.addEventListener("click", function () {
+                let stringRowCol = coins.id;
+                let row = parseInt(stringRowCol[0]);
+                let col = parseInt(stringRowCol[1]);
+
+                row = columns[col];
+
+                if (row < 0) {
+                    return;
+                }
+
+                let coin = document.getElementById(`${row}${col}`);
+                if (currentPlayer == playerOne) {
+                    currentPlayer = playerTwo;
+                    coin.style.backgroundColor = playerOne;
+                } else if (currentPlayer == playerTwo) {
+                    currentPlayer = playerOne;
+                    coin.style.backgroundColor = playerTwo;
+                }
+
+                columns[col] = columns[col] - 1;
+            });
         }
     }
 }
